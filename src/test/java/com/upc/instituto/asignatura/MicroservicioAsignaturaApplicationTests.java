d8c26f2eeca380e7c790d18e3cf3e03f00e88bbf
Luis Guisado <lguisadom@gmail.com>
2023-01-30 16:48:31 -0500

    First commit

---
+++ src/test/java/com/upc/instituto/asignatura/MicroservicioAsignaturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.instituto.asignatura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioAsignaturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
